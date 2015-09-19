47c4c0ba03e0b07aaa85853fb8bfbee3a5d35820
Bohdan <goldfinchave@gmail.com>
2015-09-19 14:29:47 +0300

    update

--- functionality.js
+++ functionality.js
@@ -1,11 +1,9 @@
 function check_user_age() {
-    "use strict";
     if (age_of_user() < 18)
     console.log("Please get the fuck aus.");
 }
 
 function age_of_user() {
-    "use strict";
     var age_text, age;
     age_text = prompt("What is your age ?");
     age = parseInt(age_text);
@@ -13,15 +11,29 @@ function age_of_user() {
 }
 
 function get_great() {
-    "use strict";
     alert("It's my " + build_great(5) + "grandmother!");
 }
 
 function build_great(depth) {
-    "use strict";
     if(depth > 0)
         return "great " + build_great(depth - 1);
     else
         return "";
+}
+
+function get_array() {
+    var todo = ["Java", "HTML", "C++", "JavaScript", "CSS", "Pascal"];
+    var priority = [3, 4, 2, 6, 5, 1];
+    var index = 0;
+    var todoSorted = [];
+
+    while (index < todo.length) {
+        var position;
+        position = priority.indexOf(index + 1);
+        todoSorted.push(todo[position]);
+        index++;
+    }
+
+    alert("Before:\n\n" + todo + "\n\n" + "After:\n\n" + todoSorted);
 }
 
