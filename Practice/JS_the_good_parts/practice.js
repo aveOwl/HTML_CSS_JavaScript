45f64a2d7841b3ba82ac0c4d9b8d06217a2f6132
Bohdan <goldfinchave@gmail.com>
2015-09-24 08:58:15 +0300

    update

--- Practice/JS_the_good_parts/practice.js
+++ Practice/JS_the_good_parts/practice.js
@@ -65,3 +65,25 @@ var sum = function() {
 
 document.writeln(sum(4, 8, 15, 16, 23, 42));
 
+
+// Exceptions
+var add = function (a, b) {
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw {
+            name: 'TypeError',
+            message: 'add needs numbers'
+        };
+    }
+    return a + b;
+}
+
+var try_it = function() {
+    try {
+        add("seven");
+    } catch(e) {
+        document.writeln(e.name + ': ' + e.message);
+    }
+}
+
+try_it();
